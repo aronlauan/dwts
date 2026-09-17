b5700086679b7c2e656ce2200e82ce7406107489
Aron Lauan <aronnlauan@gmail.com>
2026-09-16 20:55:50 -0400

    Pre ping database to minimize error messages

--- app/db.py
+++ app/db.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from app.config import SQLALCHEMY_DATABASE_URL
 
 connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args=connect_args,
+    pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
